bdf6c4234517937526f1820a38182d420ee7f61c
fairyxu <you@example.com>
2024-07-20 08:51:16 +0800

    feat:client

--- client/comfyui-api/utils/helpers/config.py
+++ client/comfyui-api/utils/helpers/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 from pathlib import Path
 current_file_path = Path(__file__).resolve()
 project_root_path = current_file_path.parents[0]
-while not (project_root_path / '.env').exists() and project_root_path != project_root_path.root:
+while not (project_root_path / 'mmqload.conf').exists() and project_root_path != project_root_path.root:
     project_root_path = project_root_path.parent
 os.environ.setdefault('INPUT_IMG_DIR', str(project_root_path) + '/input')
 os.environ.setdefault('OUTPUT_IMG_DIR',str(project_root_path) + '/output')
